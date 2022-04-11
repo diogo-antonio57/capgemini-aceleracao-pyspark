7b5840f0c1125fff6ee3d3b087478ec666b5424f
Diogo Silva <diogoantonio57@gmail.com>
2022-04-11 13:55:05 -0300

    Criação Schema e qualidade InvoiceNo, StockCode, Description

--- scripts/online-retail.py
+++ scripts/online-retail.py
@@ -2,14 +2,51 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
- 
+
+def is_null(coluna):
+	return (F.col(coluna).isNull()) | (F.col(coluna) == 'NA') | (F.col(coluna) == 'NaN')
+
+def online_retail_qa(df):
+	# Qualidade InvoiceNo
+	df = df.withColumn(
+			'InvoiceNo_qa',
+			F.when(is_null('InvoiceNo'), 'M')
+		 	 .when(~F.col('InvoiceNo').rlike('(^[0-9]{6}$)|(^C[0-9]{6}$)'), 'F')
+		)
+	
+	# Qualidade StockCode
+	df = df.withColumn(
+			'StockCode_qa',
+			F.when(is_null('StockCode'),                    'M')
+			 .when(F.col('StockCode').rlike('[a-zA-Z]'),    'A')
+			 .when(~F.col('StockCode').rlike('^[0-9]{5}$'), 'F')
+		)
+	
+	# Qualidade Description
+	df = df.withColumn(
+			'Description_qa',
+			F.when(is_null('Description'), 'M')
+		)
+
 if __name__ == "__main__":
 	sc = SparkContext()
 	spark = (SparkSession.builder.appName("Aceleração PySpark - Capgemini [Online Retail]"))
 
+	schema_online_retail = StructType([
+					  StructField('InvoiceNo', StringType(),      True),
+					  StructField('StockCode', IntegerType(),     True),
+					  StructField('Description', StringType(),    True),
+					  StructField('Quantity', IntegerType(),      True),
+					  StructField('InvoiceDate', StringType(),    True),
+					  StructField('UnitPrice', StringType(),      True),
+					  StructField('CustomerID', IntegerType(),    True),
+					  StructField('Country', StringType(),        True)
+				  ])
+
 	df = (spark.getOrCreate().read
 		          .format("csv")
 		          .option("header", "true")
-		          #.schema(schema_online_retail)
+		          .schema(schema_online_retail)
 		          .load("/home/spark/capgemini-aceleracao-pyspark/data/online-retail/online-retail.csv"))
-	print(df.show())
+	#print(df.show())
+	online_retail_qa(df)
