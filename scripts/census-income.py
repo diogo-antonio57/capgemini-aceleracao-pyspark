e428fdb5f073a1e66a9f182cab34669bce88e325
Diogo Silva <diogoantonio57@gmail.com>
2022-04-19 12:16:26 -0300

    Criação schema

--- scripts/census-income.py
+++ scripts/census-income.py
@@ -2,14 +2,33 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
- 
-if __name__ == "__main__":
-	sc = SparkContext()
-	spark = (SparkSession.builder.appName("Aceleração PySpark - Capgemini [Census Income]"))
 
-	df = (spark.getOrCreate().read
+
+if __name__ == "__main__":
+    sc = SparkContext()
+    spark = (SparkSession.builder.appName("Aceleração PySpark - Capgemini [Census Income]"))
+
+    schema_census_income = StructType([
+                    StructField('age', IntegerType(),            True),
+                    StructField('workclass', StringType(),       True),
+                    StructField('fnlwgt', IntegerType(),         True),
+                    StructField('education', StringType(),       True),
+                    StructField('education-num', IntegerType(),  True),
+                    StructField('marital-status', StringType(),  True),
+                    StructField('occupation', StringType(),      True),
+                    StructField('relationship', StringType(),    True),
+                    StructField('race', StringType(),            True),
+                    StructField('sex', StringType(),             True),
+                    StructField('capital-gain', IntegerType(),   True),
+                    StructField('capital-loss', IntegerType(),   True),
+                    StructField('hours-per-week', IntegerType(), True),
+                    StructField('native-country', StringType(),  True),
+                    StructField('income', StringType(),          True)
+                ])
+
+    df = (spark.getOrCreate().read
 		          .format("csv")
 		          .option("header", "true")
-		          #.schema(schema_census_income)
-		          .load("/home/spark/capgemini-aceleracao-pyspark/data/online-retail/census-income.csv"))
-	print(df.show())
+		          .schema(schema_census_income)
+		          .load("/home/spark/capgemini-aceleracao-pyspark/data/census-income/census-income.csv"))
+    print(df.show())
