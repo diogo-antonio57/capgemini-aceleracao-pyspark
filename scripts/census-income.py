4aed2f0bb8b3a9231cc795a418f9ea2b80937d22
Diogo Silva <diogoantonio57@gmail.com>
2022-04-20 10:49:29 -0300

    Pergunta 6

--- scripts/census-income.py
+++ scripts/census-income.py
@@ -21,6 +21,7 @@ def census_income_tr(df):
 
 
 def pergunta_1(df):
+    print('Pergunta 1')
     (df.where((F.col('income').rlike('>50K')) &
               (F.col('workclass').isNotNull()))
        .groupBy('workclass', 'income')
@@ -30,12 +31,14 @@ def pergunta_1(df):
 
 
 def pergunta_2(df):
+    print('Pergunta 2')
     (df.groupBy('race')
        .agg(F.round(F.avg('hours-per-week'), 2).alias('hora_de_trabalho_semanal_media'))
        .show())
 
 
 def pergunta_3(df):
+    print('Pergunta 3')
     (df.groupBy('sex')
        .count()
        .withColumn('percent', F.round((F.col('count')/df.count()), 2))
@@ -43,10 +46,20 @@ def pergunta_3(df):
 
 
 def pergunta_5(df):
+    print('Pergunta 5')
     (df.groupBy('occupation')
        .agg(F.round(F.avg('hours-per-week'), 2).alias('media horas/semana'))
        .orderBy(F.col('media horas/semana').desc())
        .limit(1)
+       .show())
+
+
+def pergunta_6(df):
+    print('Pergunta 6')
+    (df.groupBy('education', 'occupation')
+       .count()
+       .orderBy(F.col('count').desc())
+       .dropDuplicates(['education'])
        .show())
 
 
@@ -80,7 +93,8 @@ if __name__ == "__main__":
 
     df_tr = census_income_tr(df)
     
-    # pergunta_1(df_tr)
-    # pergunta_2(df_tr)
-    # pergunta_3(df_tr)
+    pergunta_1(df_tr)
+    pergunta_2(df_tr)
+    pergunta_3(df_tr)
     pergunta_5(df_tr)
+    pergunta_6(df_tr)
